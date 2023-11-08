3331b840207293b43496d5b7742afae0b01b920b
SiMo047 <rakibmohammed2013@gmail.com>
2023-11-08 20:29:04 +0100

    new

--- TEMA4/B DATOS/puente.php
+++ TEMA4/B DATOS/puente.php
@@ -5,7 +5,7 @@
     define('DB_NAME', 'first');  //nombre de base de datos
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'jaroso'); // contraseña de mysql 
-    define('DB_HOST', '172.17.0.2:3306'); //La IP del contenedor Mysql, y el puerto interno del contenedor
+    define('DB_HOST', 'mariadb:3306'); //La IP del contenedor Mysql, y el puerto interno del contenedor
 
     try {
         $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
